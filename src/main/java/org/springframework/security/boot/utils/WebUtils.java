6b950073733c3f7260fc6414db6f9f353b4f2de7
jeebiz <hnxyhcwdl1003@163.com>
2018-03-11 22:24:18 +0800

    修改

--- src/main/java/org/springframework/security/boot/utils/WebUtils.java
+++ src/main/java/org/springframework/security/boot/utils/WebUtils.java
@@ -27,11 +27,11 @@ public class WebUtils extends org.springframework.web.util.WebUtils {
     private static final String CONTENT_TYPE = "Content-type";
     private static final String CONTENT_TYPE_JSON = "application/json";
 
-    public static boolean isAjax(HttpServletRequest request) {
+    public static boolean isAjaxRequest(HttpServletRequest request) {
         return XML_HTTP_REQUEST.equals(request.getHeader(X_REQUESTED_WITH));
     }
-
-    public static boolean isAjax(SavedRequest request) {
+    
+    public static boolean isAjaxRequest(SavedRequest request) {
         return request.getHeaderValues(X_REQUESTED_WITH).contains(XML_HTTP_REQUEST);
     }
 
