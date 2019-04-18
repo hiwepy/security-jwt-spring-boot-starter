373327948bec28986ccbbdb077c80eaced810e1e
wandl <hnxyhcwdl1003@163.com>
2019-04-18 14:33:23 +0800

    修改依赖

--- src/main/java/org/springframework/security/boot/SecurityJwtWebAutoConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityJwtWebAutoConfiguration.java
@@ -1,12 +1,12 @@
 package org.springframework.security.boot;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AutoConfigureAfter(SecurityBizAutoConfiguration.class)
+@AutoConfigureBefore(SecurityBizAutoConfiguration.class)
 @ConditionalOnProperty(prefix = SecurityJwtProperties.PREFIX, value = "enabled", havingValue = "true")
 @EnableConfigurationProperties({ SecurityJwtProperties.class })
 public class SecurityJwtWebAutoConfiguration{
