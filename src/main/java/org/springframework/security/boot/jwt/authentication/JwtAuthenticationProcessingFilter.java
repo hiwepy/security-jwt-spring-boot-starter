a3c62ce7d22de9b0e449e9e70359eb93d4bf7210
wandl <hnxyhcwdl1003@163.com>
2019-04-24 15:57:27 +0800

    修改

--- src/main/java/org/springframework/security/boot/jwt/authentication/JwtAuthenticationProcessingFilter.java
+++ src/main/java/org/springframework/security/boot/jwt/authentication/JwtAuthenticationProcessingFilter.java
@@ -1,7 +1,7 @@
 package org.springframework.security.boot.jwt.authentication;
 
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.boot.biz.authentication.PostUsernamePasswordCaptchaAuthenticationProcessingFilter;
+import org.springframework.security.boot.biz.authentication.PostRequestAuthenticationProcessingFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * Jwt认证 (authentication)过滤器
  * @author ： <a href="https://github.com/vindell">vindell</a>
  */
-public class JwtAuthenticationProcessingFilter extends PostUsernamePasswordCaptchaAuthenticationProcessingFilter {
+public class JwtAuthenticationProcessingFilter extends PostRequestAuthenticationProcessingFilter {
 
 	public JwtAuthenticationProcessingFilter(ObjectMapper objectMapper) {
 		super(objectMapper);
