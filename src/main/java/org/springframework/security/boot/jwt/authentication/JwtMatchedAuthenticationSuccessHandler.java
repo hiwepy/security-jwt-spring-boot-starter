d6c8a9e84818b3263a79ba0b42253049a728d8f2
wandl <hnxyhcwdl1003@163.com>
2019-06-17 14:55:50 +0800

    响应信息优化

--- src/main/java/org/springframework/security/boot/jwt/authentication/JwtMatchedAuthenticationSuccessHandler.java
+++ src/main/java/org/springframework/security/boot/jwt/authentication/JwtMatchedAuthenticationSuccessHandler.java
@@ -8,9 +8,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.boot.biz.SpringSecurityBizMessageSource;
 import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
+import org.springframework.security.boot.biz.exception.AuthResponseCode;
 import org.springframework.security.boot.biz.userdetails.JwtPayloadRepository;
 import org.springframework.security.boot.biz.userdetails.SecurityPrincipal;
 import org.springframework.security.boot.utils.SubjectUtils;
@@ -25,6 +28,7 @@ import com.alibaba.fastjson.JSONObject;
  */
 public class JwtMatchedAuthenticationSuccessHandler implements MatchedAuthenticationSuccessHandler {
    
+	protected MessageSourceAccessor messages = SpringSecurityBizMessageSource.getAccessor();
 	private JwtPayloadRepository payloadRepository;
 	
 	public JwtMatchedAuthenticationSuccessHandler(JwtPayloadRepository payloadRepository) {
@@ -43,17 +47,28 @@ public class JwtMatchedAuthenticationSuccessHandler implements MatchedAuthentica
     	UserDetails userDetails = (UserDetails) authentication.getPrincipal();
     	
 		Map<String, Object> tokenMap = new HashMap<String, Object>();
-		tokenMap.put("code", "0");
+		tokenMap.put("code", AuthResponseCode.SC_AUTHC_SUCCESS.getCode());
+		tokenMap.put("msg", messages.getMessage(AuthResponseCode.SC_AUTHC_SUCCESS.getMsgKey()));
+		
 		// 账号首次登陆标记
 		if(SecurityPrincipal.class.isAssignableFrom(userDetails.getClass())) {
-			tokenMap.put("initial", ((SecurityPrincipal) userDetails).isInitial());
+			SecurityPrincipal securityPrincipal = (SecurityPrincipal) userDetails;
+			tokenMap.put("initial", securityPrincipal.isInitial());
+			tokenMap.put("alias", securityPrincipal.getAlias());
+			tokenMap.put("usercode", securityPrincipal.getUsercode());
+			tokenMap.put("userkey", securityPrincipal.getUserkey());
+			tokenMap.put("userid", securityPrincipal.getUserid());
 		} else {
 			tokenMap.put("initial", false);
+			tokenMap.put("alias", "");
+			tokenMap.put("usercode", "");
+			tokenMap.put("userkey", "");
+			tokenMap.put("userid", "");
 		}
 		tokenMap.put("perms", userDetails.getAuthorities());
 		tokenMap.put("token", getPayloadRepository().issueJwt((JwtAuthenticationToken) authentication));
 		tokenMap.put("username", userDetails.getUsername());
-
+		
 		response.setStatus(HttpStatus.OK.value());
 		response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
 		
