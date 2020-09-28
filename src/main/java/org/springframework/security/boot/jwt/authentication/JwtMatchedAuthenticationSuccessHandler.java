06424eaddb34a02e3c6aa6d0e0766518f2a0eb3f
wandl <hiwepy@163.com>
2020-09-28 13:56:53 +0800

    修改

--- src/main/java/org/springframework/security/boot/jwt/authentication/JwtMatchedAuthenticationSuccessHandler.java
+++ src/main/java/org/springframework/security/boot/jwt/authentication/JwtMatchedAuthenticationSuccessHandler.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
