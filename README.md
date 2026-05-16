976019e89d5d35a240442ccecca94e1f6a5e006c
wandl-6A72h <hiwepy@gmail.com>
2026-05-16 14:12:42 +0800

    refactor: migrate groupId and packages from com.github.hiwepy to io.github.hiwepy

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ security starter for spring boot
 
 ``` xml
 <dependency>
-	<groupId>com.github.hiwepy</groupId>
+	<groupId>io.github.hiwepy</groupId>
 	<artifactId>security-jwt-spring-boot-starter</artifactId>
 	<version>1.0.9.RELEASE</version>
 </dependency>
--- src/main/java/org/springframework/security/boot/jwt/authentication/JwtAuthorizationProvider.java
+++ src/main/java/org/springframework/security/boot/jwt/authentication/JwtAuthorizationProvider.java
@@ -1,7 +1,7 @@
 package org.springframework.security.boot.jwt.authentication;
 
-import com.github.hiwepy.jwt.JwtClaims;
-import com.github.hiwepy.jwt.JwtPayload;
+import io.github.hiwepy.jwt.JwtClaims;
+import io.github.hiwepy.jwt.JwtPayload;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
